82d1f65a7def83c8871f2d0a5a6d59888e232a85
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-02-26 12:54:30 +0300

    Modifying elements using DOM

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.getElementsByTagName("main");
+main.remove("main");
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Roy is the champion";
